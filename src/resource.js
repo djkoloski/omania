04e2b8f6a2a6bc46d662dd8894aad5128aaac5d8
David Koloski <djkoloski@gmail.com>
2015-09-14 19:23:04 -0400

    Added aliasing, messed with tile map image to help alleviate black lines

--- src/resource.js
+++ src/resource.js
@@ -1,15 +1,9 @@
 var res = {
-	TileGameResources_tmw_desert_spacing_png: "res/TileGameResources/tmw_desert_spacing.png",
-	HelloWorld_png: "res/HelloWorld.png",
-	TileGameResources__DS_Store: "res/TileGameResources/.DS_Store",
 	TileGameResources_meta_tiles_png: "res/TileGameResources/meta_tiles.png",
+	TileGameResources_tmw_desert_spacing_png: "res/TileGameResources/tmw_desert_spacing.png",
 	TileGameResources_Player_png: "res/TileGameResources/Player.png",
-	TileGameResources_hit_caf: "res/TileGameResources/hit.caf",
-	TileGameResources_TileMap_tmx: "res/TileGameResources/TileMap.tmx",
-	TileGameResources_TileMap_caf: "res/TileGameResources/TileMap.caf",
-	TileGameResources_pickup_caf: "res/TileGameResources/pickup.caf",
-	TileGameResources_move_caf: "res/TileGameResources/move.caf",
-	TileMap_tmx: "res/TileMap.tmx"
+	HelloWorld_png: "res/HelloWorld.png",
+	TileGameResources_TileMap_tmx: "res/TileGameResources/TileMap.tmx"
 };
 
 var g_resources = [];
