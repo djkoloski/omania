3a2c0c0fc48272143e784167f693df78eb3208d2
David Koloski <djkoloski@gmail.com>
2015-09-14 17:10:57 -0400

    Moved tile map into resources to fix preload bug

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,14 @@
 var res = {
+	TileGameResources_tmw_desert_spacing_png: "res/TileGameResources/tmw_desert_spacing.png",
 	HelloWorld_png: "res/HelloWorld.png",
+	TileGameResources__DS_Store: "res/TileGameResources/.DS_Store",
+	TileGameResources_meta_tiles_png: "res/TileGameResources/meta_tiles.png",
+	TileGameResources_Player_png: "res/TileGameResources/Player.png",
+	TileGameResources_hit_caf: "res/TileGameResources/hit.caf",
+	TileGameResources_TileMap_tmx: "res/TileGameResources/TileMap.tmx",
+	TileGameResources_TileMap_caf: "res/TileGameResources/TileMap.caf",
+	TileGameResources_pickup_caf: "res/TileGameResources/pickup.caf",
+	TileGameResources_move_caf: "res/TileGameResources/move.caf",
 	TileMap_tmx: "res/TileMap.tmx"
 };
 
