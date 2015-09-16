a21dd6e2242f83972c8c1b084ae4e58f8bec993d
chenj30 <chenj30@rpi.edu>
2015-09-16 16:27:45 -0400

    -Stored objects from Tiled into 2 arrays, "colliders" and "dangers" -started a file for Sultan (player character)

--- src/app.js
+++ src/app.js
@@ -1,58 +1,27 @@
 
-var HelloWorldLayer = cc.Layer.extend({
+var TMLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        /*
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
-        */
         var tileMap = new cc.TMXTiledMap("res/TileMap.tmx");
-        this.addChild(tileMap,0);
+        this.addChild(tileMap,-1);
 
-        var objects = [];
-        objects = tileMap.getObjectGroup("Bushes").getObjects();
-        for (var i = 0; i < objects.length; i++){
-            console.log(objects[i].x);
-            console.log(","+objects[i].y);
-        }
-
-        cc.log(tileMap);
+        var colliders = tileMap.getObjectGroup("Collide").getObjects();
+        var dangers = tileMap.getObjectGroup("Danger").getObjects();
 
         return true;
     }
 });
 
+
+
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new TMLayer();
         this.addChild(layer);
     }
 });
+
 
