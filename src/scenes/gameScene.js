b7be5b71fac73f1ae16eeb58103647ca11a22dc3
chenj30 <chenj30@rpi.edu>
2015-09-22 13:06:32 -0400

    Added in Danger Spawning with random

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -3,6 +3,7 @@ var GameScene = cc.Scene.extend({
 	dangerLayer: null,
 	dialogLayer: null,
 	hudLayer: null,
+	timer: null,
 	onEnter: function() {
 		this._super();
 		
@@ -10,6 +11,7 @@ var GameScene = cc.Scene.extend({
 		this.dangerLayer = new DangerLayer(this);
 		this.dialogLayer = new DialogLayer(this);
 		//this.hudLayer = new HUDLayer(this);
+		this.timer = 5;
 		
 		this.addChild(this.soldierLayer);
 		this.addChild(this.dangerLayer);
@@ -19,5 +21,15 @@ var GameScene = cc.Scene.extend({
 		this.scheduleUpdate();
 	},
 	update: function(dt) {
+		if (this.timer > 0.0) {
+			this.timer -= dt;
+			if (this.timer <= 0.0) {
+				var randDanger = Math.trunc(Math.random() * 6);
+				var randTime = Math.trunc((Math.random() * 2) + 1);
+				console.log(randDanger,randTime);
+				this.dangerLayer.spawn(randDanger, randTime);
+				this.timer = 5;
+			}
+		}
 	}
 });
