801ce275cd50620d6f9f472c29289432718fbcf1
chenj30 <chenj30@rpi.edu>
2015-09-23 03:50:18 -0400

    added pictures to end scene

--- src/scenes/endScene.js
+++ src/scenes/endScene.js
@@ -1,7 +1,5 @@
 var EndGameLayer = cc.Layer.extend({
-    BACKGROUND: res.floor_png,
-    TITLE: res.floor_png,
-    TEXT: res.floor_png,
+    BACKGROUND: res.END_SCREEN_png,
     ctor: function(scene) {
         this._super();
 
@@ -16,19 +14,9 @@ var EndGameLayer = cc.Layer.extend({
         cc.eventManager.addListener(keyListener, this);
 
         var background = new cc.Sprite(EndGameLayer.prototype.BACKGROUND);
-        //var background = new cc.LabelTTF("GAME\nOVER", "Courier New", 144);
-        //background.setColor(cc.color(255,230,225,1));
         background.setPosition(cc.winSize.width/2, cc.winSize.height/2);
-        //var title = new cc.Sprite(StartLayer.prototype.TITLE);
-        var title = new cc.LabelTTF("YOU WIN", "Courier New", 144);
-        title.setPosition(cc.winSize.width/2, 3*cc.winSize.height/4);
-        var endText = new cc.LabelTTF("Press R to Replay", "Arial", 38);
-        //var gameOverLabel = new cc.Sprite(GameOverLayer.prototype.TEXT);
-        endText.setPosition(cc.winSize.width/2, cc.winSize.height/4);
 
         this.addChild(background, 4);
-        this.addChild(title,5);
-        this.addChild(endText, 6);
     },
     endGame: function(key){
         if (key == cc.KEY.r) {
@@ -42,7 +30,7 @@ var EndGameLayer = cc.Layer.extend({
 var EndScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new EngGameLayer();
+        var layer = new EndGameLayer();
         this.addChild(layer);
     }
 });
