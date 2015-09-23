10d49a721f19a712684323225d7eae0e9fef10aa
David Koloski <djkoloski@gmail.com>
2015-09-22 22:02:14 -0400

    Added animations on left and right dangers

--- src/curve.js
+++ src/curve.js
@@ -1,3 +1,21 @@
+function BounceCurve(time, curve) {
+	this.time = time;
+	this.curve = curve;
+	this.elapsedTime = 0.0;
+}
+BounceCurve.prototype.update = function(dt) {
+	this.elapsedTime = Math.min(this.time, this.elapsedTime + dt);
+};
+BounceCurve.prototype.getValue = function() {
+	if (this.elapsedTime * 2.0 < this.time)
+		return this.curve.evaluate(this.elapsedTime * 2.0 / this.time);
+	else
+		return this.curve.evaluate(2.0 - this.elapsedTime * 2.0 / this.time);
+};
+BounceCurve.prototype.isFinished = function() {
+	return this.elapsedTime >= this.time;
+};
+
 function AnimationCurve(time, curve) {
 	this.time = time;
 	this.curve = curve;
@@ -8,6 +26,17 @@ AnimationCurve.prototype.update = function(dt) {
 };
 AnimationCurve.prototype.getValue = function() {
 	return this.curve.evaluate(this.elapsedTime / this.time);
+};
+BounceCurve.prototype.isFinished = function() {
+	return this.elapsedTime >= this.time;
+};
+
+function HalfSigmoid(k) {
+	// k should range from -1 to 1
+	this.k = k;
+}
+HalfSigmoid.prototype.evaluate = function(t) {
+	return (this.k * t - t) / (2.0 * this.k * t - this.k - 1.0);
 };
 
 function Sigmoid(k) {
