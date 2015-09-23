182096c3fb0f284ef185b8884769b6059bb44003
chenj30 <chenj30@rpi.edu>
2015-09-23 04:09:37 -0400

    fixed up dialog

--- src/dialogs.js
+++ src/dialogs.js
@@ -2,7 +2,7 @@ var DIALOGS = [
 	[
 		{
 			speaker: 'Talha',
-			message: 'Excuse me- I didn\'t mean to bump into you.'
+			message: 'Excuse me-I didn\'t mean to bump into you.'
 		},
 		{
 			speaker: 'Aazaad',
@@ -10,7 +10,8 @@ var DIALOGS = [
 		},
 		{
 			speaker: 'Talha',
-			message: 'No, this is my first. When the army came through my town and I heard Sultan bin Saif speak of tearing Oman free from Portugal\'s grip, I could not stay still.'
+			message: 'No, this is my first. When the army came through my town, I heard Sultan bin Saif spoke of' +
+			' tearing Oman free from Portugal\'s grip. I could not stay still.'
 		},
 		{
 			speaker: 'Aazaad',
@@ -20,11 +21,12 @@ var DIALOGS = [
 	[
 		{
 			speaker: 'Aazaad',
-			message: 'Sultan bin Saif will shout a warning to us to either move to up, down, left, right, or tight in the center, so that the army is not hit by obstacles.'
+			message: 'Sultan bin Saif will shout a warning to us to either move to up, down, left, right, or' +
+			' stay tight in the center, so that the army is not hit by hazards.'
 		},
 		{
 			speaker: null,
-			message: 'Press W, A, S, D, or Spacebar to command the soldiers'
+			message: 'Hold W, A, S, D, to change the soldiers\' formation to Top, Left, Bottom, and Right respectively'
 		}
 	],
 	[
@@ -38,13 +40,13 @@ var DIALOGS = [
 		},
 		{
 			speaker: 'Aazaad',
-			message: 'And sacked all the Portuguese forts before this one. A victory here will mean a free Oman- a failure could give the Portuguese time to recover and doom our cause.'
+			message: 'And sacked all the Portuguese forts before this one. A victory here will mean a free Oman-a failure could give the Portuguese time to recover and doom our cause.'
 		}
 	],
 	[
 		{
 			speaker: 'Aazaad',
-			message: 'Keep up son!'
+			message: 'Keep it up son!'
 		},
 		{
 			speaker: 'Talha',
@@ -72,7 +74,7 @@ var DIALOGS = [
 	[
 		{
 			speaker: 'Aazaad',
-			message: 'We are to the end of our march- the cowardly Portuguese have fled to the sea and Fort Capitan, but I believe that the outsiders will have surrendered within a year. A new age has indeed dawned, and it is not only in this nation that Oman\'s power will shine forth. Peaceful at home, powerful abroad, that is what Sultan bin Saif will pursue. And we will not stop until the threat of Portugal no longer exists.'
+			message: 'We are to the end of our march-the cowardly Portuguese have fled to the sea and Fort Capitan, but I believe that the outsiders will have surrendered within a year. A new age has indeed dawned, and it is not only in this nation that Oman\'s power will shine forth. Peaceful at home, powerful abroad, that is what Sultan bin Saif will pursue. And we will not stop until the threat of Portugal no longer exists.'
 		},
 		{
 			speaker: null,
